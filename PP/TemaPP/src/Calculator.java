58562b0f54758944c69713b799aebc74d6f1bc2b
EfexOny <irimiamihai2006@yahoo.com>
2026-02-26 10:01:16 +0200

    merge

--- PP/TemaPP/src/Calculator.java
+++ PP/TemaPP/src/Calculator.java
@@ -4,158 +4,115 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
+import java.util.Stack;
+
+
 
 public class Calculator extends JFrame {
 
-
     JButton digits[] = {
-            new JButton(" 0 "),
-            new JButton(" 1 "),
-            new JButton(" 2 "),
-            new JButton(" 3 "),
-            new JButton(" 4 "),
-            new JButton(" 5 "),
-            new JButton(" 6 "),
-            new JButton(" 7 "),
-            new JButton(" 8 "),
+            new JButton(" 0 "), new JButton(" 1 "), new JButton(" 2 "),
+            new JButton(" 3 "), new JButton(" 4 "), new JButton(" 5 "),
+            new JButton(" 6 "), new JButton(" 7 "), new JButton(" 8 "),
             new JButton(" 9 ")
-
     };
 
     JButton operators[] = {
-            new JButton(" + "),
-            new JButton(" - "),
-            new JButton(" * "),
-            new JButton(" / "),
-            new JButton(" = "),
-            new JButton(" C "),
-            new JButton(" )"),
-            new JButton(" ( ")
-
+            new JButton(" + "), new JButton(" - "), new JButton(" * "),
+            new JButton(" / "), new JButton(" = "), new JButton(" C "),
+            new JButton(" ) "), new JButton(" ( ")
     };
 
-    String oper_values[] = {"+", "-", "*", "/", "=", "",")","("};
-
-    String value;
-    char operator;
-
+    String oper_values[] = {"+", "-", "*", "/", "=", "", ")", "("};
     JTextArea area = new JTextArea(3, 5);
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.setSize(500, 500);
+        calculator.setSize(400, 500);
         calculator.setTitle(" Java-Calc-Extended, PP Lab1 ");
         calculator.setResizable(true);
         calculator.setVisible(true);
         calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-//        System.out.println(EvaluareExpresie(String.valueOf(FormaPoloneza("1+2/2+(2-3*4)"))));
-
-//2*(4-5/6)/456)
     }
 
-
-    /*
-        GradSemn(char var)
-        -return int
-
-        Determina daca semnul extras mai devreme este mai mic ca si prioritate fata de cel din stiva
-        Returneaza 1 daca semnul extras e mai mic in prioritate altfel returneaza 0
+    /**
+     * Returneaza precedenta operatorilor.
+     * Parantezele au prioritate 0 in interiorul stivei pentru a nu scoate alti operatori.
      */
-
-    public static int GradSemn(char semn){
+    public static int GradSemn(char semn) {
         switch (semn) {
-            case ')': case '(': return 0;
             case '+': case '-': return 1;
             case '*': case '/': return 2;
+            case '(': case ')': return 0;
             default: return -1;
         }
     }
 
-    /*
-        Evalueaza forma poloneza si returneaza tot string sa poata fi afisat pe calculator
-
-     parcurgi expresie de la stanga la dr ca nu am dat return la rezultat.reverse()
-        numerele le pui in stack iar cand gasesti un semn iei din stack 2 numere primul fiind A iar nd B
-        si faci operatia aferenta dupa pui inapoi in stack
-     */
-
-    public static String Operatie(char op,double n1,double n2){
-        String rez=new String();
-
-        switch (op){
-            case '+':
-                rez=String.valueOf(n1+n2);
-                break;
-            case '-':
-                rez=String.valueOf(n1-n2);
-                break;
-            case '*':
-                rez=String.valueOf(n1*n2);
-                break;
-            case '/':
-                rez=String.valueOf(n1/n2);
-                break;
+    public static String Operatie(char op, double n1, double n2) {
+        switch (op) {
+            case '+': return String.valueOf(n1 + n2);
+            case '-': return String.valueOf(n1 - n2);
+            case '*': return String.valueOf(n1 * n2);
+            case '/': return n2 != 0 ? String.valueOf(n1 / n2) : "0";
+            default: return "0";
         }
-        return rez;
     }
 
-    public static String EvaluareExpresie(String expresie){
-        String rez= "";
-        Stiva stack= new Stiva();
+    /**
+     * Evalueaza expresia in forma postfixata (poloneza).
+     * Ordinea operanzilor: n2 este operandul din stanga, n1 cel din dreapta.
+     */
+    public static String EvaluareExpresie(String expresie) {
+        if (expresie.isEmpty()) return "";
+        Stiva stack = new Stiva();
 
-        for(int i=0;i<=expresie.length()-1;i++){
-                char c=expresie.charAt(i);
-                if(Character.isDigit(c)) {
-                    stack.push(String.valueOf(c));
-                }else {
-                    char op=c;
-                    double n1,n2;
-                    n1=Double.parseDouble(stack.top());
-                    stack.pop();
-                    n2=Double.parseDouble(stack.top());
-                    stack.pop();
-                    rez=Operatie(op,n1,n2);
-                    stack.push(rez);
-                }
+        for (int i = 0; i < expresie.length(); i++) {
+            char c = expresie.charAt(i);
+            if (Character.isDigit(c)) {
+                stack.push(String.valueOf(c));
+            } else {
+                if (stack.IsEmpty()) continue;
+                double n1 = Double.parseDouble(stack.top()); stack.pop();
+                if (stack.IsEmpty()) return String.valueOf(n1);
+                double n2 = Double.parseDouble(stack.top()); stack.pop();
+                stack.push(Operatie(c, n2, n1));
+            }
         }
-
-        return rez;
+        return stack.top();
     }
 
-    /*
-        Construieste forma poloneza
+    /**
+     * Transforma infix in postfix
+     * Parcurgere standard de la stanga la dreapta.
      */
+    public static StringBuilder FormaPoloneza(String expresiaInfixata) {
+        StringBuilder rezultat = new StringBuilder();
+        Stiva stack = new Stiva();
 
-    public static StringBuilder FormaPoloneza(String expresiaInfixata){
-        StringBuilder rezultat=new StringBuilder();
-        Stiva stack=new Stiva();
-
-        for(int i=expresiaInfixata.length() -1 ;i>=0;i--) {
+        for (int i = 0; i < expresiaInfixata.length(); i++) {
             char c = expresiaInfixata.charAt(i);
-            // daca e cifra o pui direct
-            if(Character.isDigit(c)) {
+
+            if (Character.isDigit(c)) {
                 rezultat.append(c);
-            }else if(c=='('){
-                while(!stack.IsEmpty() && !stack.top().equals(")")){
+            } else if (c == '(') {
+                stack.push(String.valueOf(c));
+            } else if (c == ')') {
+                while (!stack.IsEmpty() && !stack.top().equals("(")) {
                     rezultat.append(stack.top());
                     stack.pop();
                 }
-                stack.pop();//scapi de )
-            }else {
-                if(!stack.IsEmpty() && GradSemn(c) < GradSemn(stack.top().charAt(0))){
-                    while (!stack.IsEmpty() && GradSemn(c) < GradSemn(stack.top().charAt(0))) {
-                        rezultat.append(stack.top());
-                        stack.pop();
-                        stack.push(String.valueOf(c));
-                    }
+                if (!stack.IsEmpty()) stack.pop(); // Elimina "("
+            } else {
+                // Operator: scoate din stiva operatorii cu prioritate mai mare sau egala
+                while (!stack.IsEmpty() && !stack.top().equals("(") &&
+                        GradSemn(c) <= GradSemn(stack.top().charAt(0))) {
+                    rezultat.append(stack.top());
+                    stack.pop();
                 }
-                else{
-                    stack.push(String.valueOf(c));
-                }
+                stack.push(String.valueOf(c));
             }
         }
-        while(!stack.IsEmpty()){
+        while (!stack.IsEmpty()) {
             rezultat.append(stack.top());
             stack.pop();
         }
@@ -167,55 +124,35 @@ public class Calculator extends JFrame {
         JPanel buttonpanel = new JPanel();
         buttonpanel.setLayout(new FlowLayout());
 
-        for (int i=0;i< digits.length;i++)
-            buttonpanel.add(digits[i]);
-
-
-        for (int i=0;i<operators.length;i++)
-            buttonpanel.add(operators[i]);
+        for (JButton b : digits) buttonpanel.add(b);
+        for (JButton b : operators) buttonpanel.add(b);
 
         add(buttonpanel, BorderLayout.CENTER);
-        area.setForeground(Color.BLACK);
-        area.setBackground(Color.WHITE);
         area.setLineWrap(true);
         area.setWrapStyleWord(true);
         area.setEditable(false);
 
-        for (int i=0;i<digits.length;i++) {
+        // ActionListeners pentru cifre
+        for (int i = 0; i < digits.length; i++) {
             int finalI = i;
-            digits[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent actionEvent) {
-                    area.append(Integer.toString(finalI));
-                }
-            });
+            digits[i].addActionListener(e -> area.append(Integer.toString(finalI)));
         }
 
-        for (int i=0;i<operators.length;i++){
+        for (int i = 0; i < operators.length; i++) {
             int finalI = i;
-            operators[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent actionEvent) {
-                    if (finalI == 5)
-                        area.setText("");
-                    else
-                    if (finalI == 4) {
-                        String expresie,exprPoloneza;
-                        try {
-
-                            expresie=area.getText();
-                            exprPoloneza=String.valueOf(FormaPoloneza(expresie));
-
-                            area.setText(EvaluareExpresie(exprPoloneza));
-
-                        } catch (Exception e) {
-                            area.setText(" !!!Probleme!!! ");
-                        }
+            operators[i].addActionListener(e -> {
+                if (finalI == 5) { // Butonul 'C'
+                    area.setText("");
+                } else if (finalI == 4) { // Butonul '='
+                    try {
+                        String infix = area.getText();
+                        String postfix = FormaPoloneza(infix).toString();
+                        area.setText(EvaluareExpresie(postfix));
+                    } catch (Exception ex) {
+                        area.setText(" Eroare ");
                     }
-                    else {
-                        area.append(oper_values[finalI]);
-                        operator = oper_values[finalI].charAt(0);
-                    }
+                } else { // Operatori si paranteze
+                    area.append(oper_values[finalI]);
                 }
             });
         }
