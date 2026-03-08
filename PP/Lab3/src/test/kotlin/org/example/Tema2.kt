dffd1e4a9dd52580636321530abcce1cc804aa7a
EfexOny <irimiamihai2006@yahoo.com>
2026-03-08 10:31:22 +0200

    tema 2

--- PP/Lab3/src/test/kotlin/org/example/Tema2.kt
+++ PP/Lab3/src/test/kotlin/org/example/Tema2.kt
@@ -1,6 +1,49 @@
 package org.example
 
-class Lab3Tema2(){
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import kotlin.test.Test
+
+fun <T> proc(text : T): String{
+    val rez=text.toString()
+    return rez
+        .replace(Regex("""\s+\d+\s+"""), "\n")
+        .replace(Regex(" +")," ")
+        .replace(Regex("""\\n+"""),"\n")
 
 
+}
+
+@RunWith(Parameterized::class)
+class Lab3Tema2(
+    private val input: String,
+){
+
+    companion object{
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<String>> {
+            return listOf(
+                arrayOf("""
+                        Capitolul 1: Introducere
+                        
+                        
+                        Aceasta este o propoziție    cu multe spații inutile.
+                        
+                        
+                              24
+                    
+                        """
+                )
+            )
+        }
+
+    }
+
+
+    @Test
+    fun testManipulareEbook(){
+        print(proc(input))
+
+    }
 }
