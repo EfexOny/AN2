dc0c7fef66ad43dd21b7a9ab3a9046c2171331bc
EfexOny <irimiamihai2006@yahoo.com>
2026-03-21 17:37:54 +0200

    pctB

---
+++ PA/lab5/pctB.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+
+using namespace std;
+
+// arbore
+
+struct arbore{
+    int data;
+    arbore *stg,*dr;
+};
+
+// lista de arbori
+
+struct lista{
+    lista *next;
+    arbore *arb;
+};
+
+void addLista(lista *&curr,arbore *toInsert){
+    lista *parc=curr;
+
+    lista *nou=new lista;
+    nou->arb=toInsert;
+    nou->next=nullptr;
+
+    if(curr==nullptr || toInsert->data < curr->arb->data ){
+
+        nou->next=curr;
+        curr=nou;
+
+        return;
+    }
+
+        // crescator
+        while(parc->next && parc->next->arb->data < toInsert->data && parc->next){
+            parc=parc->next;
+        }
+        nou->next=parc->next;
+        parc->next=nou;
+}
+
+arbore* removeLowest(lista *&list){
+    
+    // return la primu elem din lista mereu ca e ordonata
+    arbore *ret=nullptr;
+    if(list){
+        ret=list->arb;
+        list=list->next;
+    }
+    return ret;
+
+}
+
+void afisareLista( lista *l){
+    lista *parc;
+    if(l){
+        parc=l;
+    }
+    while(parc){
+        cout<<parc->arb->data<<" ";
+        parc=parc->next;
+    }
+
+    cout<<endl;
+}
+
+int nrElem(lista *l){
+    int rez=0;
+    while(l){
+        l=l->next;
+        rez++;
+    }
+    return rez;
+}
+
+void preordArbore(arbore *arb){
+    if(!arb){
+        return;
+    }
+    cout<<arb->data<<" ";
+    preordArbore(arb->stg);
+    preordArbore(arb->dr);
+}
+
+void lep(lista *& l){
+    // extragi cei mai mici 2 arbori pana cand ai un singur element in lista ?
+    while(nrElem(l) > 1){
+        arbore *l1,*l2;
+        l1=removeLowest(l);
+        l2=removeLowest(l);
+        // faci un arobre nou si rad->data = data la cei 2 arbori
+            // stg si dr sunt cei 2 arbori - arb1-> stg arb2-> dr
+
+        arbore *nou=new arbore;
+        nou->data=l1->data+l2->data;
+        nou->stg=l1;
+        nou->dr=l2;
+
+
+        // pui arborele nou creata in lista
+        addLista(l,nou);
+    }
+}
+
+
+
+
+
+
+int main(){
+
+    lista *l=nullptr;
+    arbore *a1,*a2,*a3,*a4,*a5;
+    a1=new arbore;a2=new arbore;a3=new arbore;a4=new arbore;a5=new arbore;
+    a1->data=15;a2->data=5;a3->data=3;a4->data=10;a5->data=9;
+
+    addLista(l,a1);addLista(l,a2);addLista(l,a3);addLista(l,a4);addLista(l,a5);
+
+    
+    afisareLista(l);
+
+
+
+    lep(l);
+
+    afisareLista(l);
+    preordArbore(l->arb);
+    return 0;
+}
---
+++ PA/lab5/pctC.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+// arbore
+
+struct arbore{
+    int data;
+    arbore *stg,*dr;
+};
+
+// lista de arbori
+
+struct minHeap{
+    minHeap *stg,*dr;
+    arbore *arb;
+};
+
+
+
+
+
+int main(){
+
+    return 0;
+}
