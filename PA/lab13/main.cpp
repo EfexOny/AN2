e2d54cb3f381ff8b3688c45e8544cb4216fc7b1b
EfexOny <irimiamihai2006@yahoo.com>
2026-05-21 19:03:59 +0300

    push

--- PA/lab13/main.cpp
+++ PA/lab13/main.cpp
@@ -1,90 +1,88 @@
-/*
-    i- nod start
-    
-
-*/
 #include <iostream>
-
-#include <iostream>
-#include <cmath>
-#define inf (int)INFINITY
 
 using namespace std;
 
+#define INF 352
 
-void afisare(int p[4][4], int source, int dest){
-    if(p[source][dest] == source){
-        cout<<(char)(source + 'A')<< "->"<< (char)(dest + 'A');
+const int N = 5;     
+
+void afisare(int p[N][N], int source, int dest) {
+    if (p[source][dest] == source) {
+        cout << source << "->" << dest;
         return;
-    }else{
+    } else {
         afisare(p, source, p[source][dest]);
-        cout<<"->"<<(char)(dest + 'A');
+        cout << "->" << dest;
     }
 }
 
-
-void Floyd_Warshall(int l[4][4],int source, int dest){
-
-    int p[4][4];
-    for(int i =0; i<4; i++){
-        for (int j = 0; j < 4; j++){
-            if(i != j && l[i][j] != inf){
+void Floyd_Warshall(int l[N][N], int source, int dest) {
+    int p[N][N];
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i != j && l[i][j] != INF) {
                 p[i][j] = i;
+            } else {
+                p[i][j] = -1;
             }
-            else
-            p[i][j] = -1;
         }
     }
 
-
-    for(int k = 0; k < 4; k++){
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 int temp;
-                if((l[i][k] == inf) || (l[k][j] == inf)){
-                     temp = inf;
-                }else{
+                if ((l[i][k] == INF) || (l[k][j] == INF)) {
+                    temp = INF;
+                } else {
                     temp = l[i][k] + l[k][j]; 
                 }
 
-                if(temp < l[i][j]){
+                if (temp < l[i][j]) {
                     l[i][j] = temp;
                     p[i][j] = p[k][j];
                 }
 
-                if((i == j) && l[i][j] < 0){
-                    cout<<"graful are circuite negative";
-                    return ;
+                if ((i == j) && l[i][j] < 0) {
+                    cout << "Graful are circuite negative\n";
+                    return;
                 }
             }
         }
     }
     afisare(p, source, dest);
+    cout << "\n";
 }
 
+int main() {
+    int l[N][N];
 
-
-
-int main(){
-         int source, dest;
-    source = 0;
-    dest = 2;
-
-    int l[4][4]={inf};
-
-    l[0][0] = 0;
-    l[0][1] = 2;
-    l[0][3] = 3;
-    l[1][0] = 3;
-    l[1][2] = 2;
-    l[2][2] = 0;
-    l[2][3] = 4;
-    l[3][1] = -2;
-    l[3][1] = 6;
-    l[3][3] = 0;
-
-
-
-Floyd_Warshall(l, source, dest );
     
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == j) {
+                l[i][j] = 0;
+            } else {
+                l[i][j] = INF;
+            }
+        }
+    }
+
+    l[0][1] = 6;
+    l[0][3] = 1;
+
+    l[1][4] = 4; 
+    l[1][2] = 5; 
+
+    l[2][4] = 5;
+
+    l[3][4] = 1;
+    l[3][1] = 2;
+    
+    l[4][1] = 2;
+    
+    Floyd_Warshall(l, 0, 4);
+    
+    return 0;
 }
